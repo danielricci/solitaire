805aea8165d47c1df628b01c0bc3ba2a24db697d
Daniel Ricci <thedanny09@icloud.com>
2018-11-07 22:42:38 -0500

    Code cleanup and added border on selection of a card deck

--- src/game/views/DeckSelectionDialogView.java
+++ src/game/views/DeckSelectionDialogView.java
@@ -24,15 +24,24 @@
 
 package game.views;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import framework.core.mvc.view.DialogView;
@@ -51,24 +60,15 @@ import generated.DataLookup;
  */
 public final class DeckSelectionDialogView extends DialogView {
 
+    /**
+     * The number of rows in the card list
+     */
     private final int _cardRows = 2;
     
-    private final int _cardColumns = 6;
-    
-    private final JPanel _drawPanel = new JPanel(new GridBagLayout());
-    
-    private final GridBagConstraints _drawPanelConstraints = new GridBagConstraints();
-    
-    private final JPanel _actionsPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-    
-    private final JButton _okButton = new JButton("OK");
-    
-    private final JButton _cancelButton = new JButton("Cancel");
-    
     /**
-     * Indicates if the cumulative score option has changed since it's last saved value
+     * The number of columns in the card list
      */
-    public boolean cumulativeScoreChanged;
+    private final int _cardColumns = 6;
     
     /**
      * Constructs a new instance of this class type
@@ -88,23 +88,32 @@ public final class DeckSelectionDialogView extends DialogView {
         DataLookup.BACKSIDES[] backsides = DataLookup.BACKSIDES.values();
         
         // Set the initial constraints for the grid bag layout
-        _drawPanelConstraints.weighty = 1;
-        _drawPanelConstraints.weightx = 1;
-        _drawPanelConstraints.insets = new Insets(5, 5, 5, 5);
+        GridBagConstraints drawPanelConstraints = new GridBagConstraints();
+        drawPanelConstraints.weighty = 1;
+        drawPanelConstraints.weightx = 1;
+        drawPanelConstraints.insets = new Insets(5, 5, 5, 5);
+        
+        List<JButton> buttons = new ArrayList<JButton>();
+        
+        // The panel that holds the list of cards
+        JPanel cardPanel = new JPanel(new GridBagLayout());
         
         // Go through card rows and card columns, and populate each index with a JButton
         // containing one of the card images
         for(int row = 0, index = 0; row < _cardRows; ++row) {
-            _drawPanelConstraints.gridy = row;
+            drawPanelConstraints.gridy = row;
             for(int column = 0; column < _cardColumns; ++column, ++index) {
                 
                 // Create the button and set the size we want it to be
                 JButton button = new JButton();
+                button.setContentAreaFilled(false);
+                button.setFocusPainted(false);
                 button.setPreferredSize(new Dimension(39, 68));
                 
                 // Create the backside entity and assign it to the button
                 BacksideCardEntity entity = new BacksideCardEntity(backsides[index]);
                 button.putClientProperty(button, entity);
+                
                 
                 // Set the icon of the button, make sure to scale it appropriately
                 button.setIcon(new ImageIcon(entity.getRenderableContent().getScaledInstance(
@@ -113,21 +122,51 @@ public final class DeckSelectionDialogView extends DialogView {
                     java.awt.Image.SCALE_SMOOTH
                 )));
                 
+                // Add a listener event for when the deck image is selected
+                button.addMouseListener(new MouseAdapter() {
+                    @Override public void mousePressed(MouseEvent event) {
+                        buttons.forEach(z -> z.setBorder(null));
+                        button.setBorder(BorderFactory.createLineBorder(Color.BLUE, 2));
+                    }
+                });
+                
+                // Add a reference to the list of buttons
+                buttons.add(button);
+                
                 // Specify the proper column constraint
-                _drawPanelConstraints.gridx = column;
+                drawPanelConstraints.gridx = column;
                 
                 // Add the button to the panel
-                _drawPanel.add(button, _drawPanelConstraints);
+                cardPanel.add(button, drawPanelConstraints);
             }
         }
+        
+        // The Cancel button        
+        JButton cancelButton = new JButton("Cancel");
+        cancelButton.addActionListener(new ActionListener() {
+            @Override public void actionPerformed(ActionEvent event) {
+                setDialogResult(JOptionPane.CANCEL_OPTION);
+                setVisible(false);
+            }
+        });
+        
+        // The Cancel button
+        JButton okButton = new JButton("OK");
+        okButton.addActionListener(new ActionListener() {
+            @Override public void actionPerformed(ActionEvent event) {
+                setDialogResult(JOptionPane.OK_OPTION);
+                setVisible(false);
+            }
+        });
        
         // Add the okay and cancel buttons
-        _actionsPanel.add(_okButton);
-        _actionsPanel.add(_cancelButton);
+        JPanel actionsPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        actionsPanel.add(okButton);
+        actionsPanel.add(cancelButton);
         
         // Add the main housing panels to this dialog
-        add(_drawPanel);
-        add(_actionsPanel);
+        add(cardPanel);
+        add(actionsPanel);
         
         // Pack the UI to fit
         pack();
