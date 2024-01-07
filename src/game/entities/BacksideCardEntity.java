9958674b23d05f95fd242a33f230bf9c5a5f0aa1
Daniel Ricci <thedanny09@icloud.com>
2024-01-07 04:09:46 -0500

    Updated License, Updated data to include missing animations
    - Added the missing animations (Beach, Castle, Robot) from the new
    generated data
    - Updated code to account for changes done in DataEntity
    - For this build you must use mead-framework version 58ceaad or later
    because I haven't yet generated a .jar for mead
    - Added support for "scenes", the ability to be able to animate the
    portions of the card backside image
    - Removed the license associated to this project. Just like for mead,
    you need my permission to use my code moving forward

--- src/game/entities/BacksideCardEntity.java
+++ src/game/entities/BacksideCardEntity.java
@@ -1,68 +1,159 @@
-/**
- * MIT License
- * 
- * Copyright (c) 2019 Daniel Ricci {@literal <thedanny09@icloud.com>}
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 package game.entities;
 
-import framework.core.entity.AbstractDataEntity;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import framework.communication.internal.signal.arguments.EventArgs;
+import framework.core.entity.DataEntity;
+import framework.core.factories.AbstractFactory;
+import framework.core.factories.ViewFactory;
+import framework.core.graphics.IRenderable;
 
 import game.config.OptionsPreferences;
+import game.views.StockView;
 
 import generated.DataLookup;
 
+public class BacksideCardEntity extends DataEntity {
+	private class SceneRenderer implements IRenderable {
+		private Image deckImage;
+		private Image deckImageAnimation = null;
+		private List<Image> _deckImageAnimations = new ArrayList<Image>();
+		public boolean enabled = false;
 
-public class BacksideCardEntity extends AbstractDataEntity {
+		private int delay = 0;
+		private int delta = 1000;
 
-    public static final String DECK_BACKSIDE_UPDATED = "DECK_BACKSIDE_UPDATED";
-    
-    private boolean _isBacksideVisible = false;
+		private Timer timer;
 
-    private DataLookup.BACKSIDES _backside;
-    
-    public BacksideCardEntity() {
-        setBackside();
-    }
-    
-    public void setIsBacksideShowing(boolean isShowing) {
-        _isBacksideVisible = isShowing;
-    }
-    
-    public boolean getIsBacksideShowing() {
-        return _isBacksideVisible;
-    }
-    
-    public DataLookup.BACKSIDES getBacksideData() {
-        return _backside;
-    }
-    
-    private void setBackside() {
-        OptionsPreferences preferences = new OptionsPreferences();
-        preferences.load();
-        _backside = preferences.deck;
-        super.setActiveData(preferences.deck.identifier);
-    }
+		private void clear() {
+			if (this.timer != null) {
+				this.timer.cancel();
+				this.timer = null;
+			}
+			this.deckImage = null;
+			this.deckImageAnimation = null;
+			this._deckImageAnimations = null;
+		}
+		
+		public void setScene(Image deckImage, List<Image> deckImageAnimations, int timer) {
+			this.clear();
 
-    @Override public void refresh() {
-        setBackside();
-    }
+			this.deckImage = deckImage;
+			this._deckImageAnimations = deckImageAnimations;
+ 
+			this.timer = new Timer(true);
+			this.timer.schedule(new TimerTask() {
+				private int index = 0;
+
+				@Override public void run() {
+					deckImageAnimation = _deckImageAnimations.get(index);
+					index = (index + 1) % _deckImageAnimations.size();
+					AbstractFactory.getFactory(ViewFactory.class).multicastSignalListeners(StockView.class, new EventArgs(this, DECK_ANIMATION_UPDATED));
+				}
+			}, delay, delta);
+			this.enabled = true;
+		}
+
+		@Override public Image getRenderableContent() {
+			if (!this.enabled) {
+				return null;
+			}
+
+			if (this.deckImage == null) {
+				return null;
+			}
+
+			if (this._deckImageAnimations == null || this._deckImageAnimations.isEmpty()) {
+				return deckImage;
+			}
+
+			BufferedImage bufferedImage = new BufferedImage(deckImage.getWidth(null), deckImage.getHeight(null),
+					BufferedImage.TYPE_INT_ARGB);
+			Graphics2D graphics = bufferedImage.createGraphics();
+			graphics.drawImage(deckImage, 0, 0, null);
+			if (this.deckImageAnimation != null) {
+				graphics.drawImage(this.deckImageAnimation, 32, 32, null);
+			}
+
+			return bufferedImage;
+		}
+	}
+
+	private final SceneRenderer sceneRenderer = new SceneRenderer();
+
+	public static final String DECK_BACKSIDE_UPDATED = "DECK_BACKSIDE_UPDATED";
+
+	public static final String DECK_ANIMATION_UPDATED = "DECK_ANIMATION_UPDATED";
+
+	private boolean isBacksideVisible = false;
+
+	private DataLookup.BACKSIDES _backside;
+
+	public BacksideCardEntity() {		
+		this.setBackside();
+	}
+	
+	public void setIsBacksideShowing(boolean isShowing) {
+		isBacksideVisible = isShowing;
+	}
+
+	public boolean getIsBacksideShowing() {
+		return isBacksideVisible;
+	}
+
+	public DataLookup.BACKSIDES getBacksideData() {
+		return _backside;
+	}
+
+	private void setBackside() {
+		OptionsPreferences preferences = new OptionsPreferences();
+		preferences.load();
+		
+		if (_backside == preferences.deck) {
+			return;
+		}
+		_backside = preferences.deck;
+		super.setActiveData(preferences.deck.identifier);
+		
+		switch (_backside) {
+		case DECK_7:
+			break;
+		case DECK_10:
+			break;
+		case DECK_11:
+			break;
+		case DECK_12:
+			this.sceneRenderer.setScene(super.getRenderableContent(),
+					Arrays.asList(
+							null,
+							new DataEntity(DataLookup.ANIMATIONS.POKER_1.identifier).getRenderableContent(), 
+							new DataEntity(DataLookup.ANIMATIONS.POKER_2.identifier).getRenderableContent(), 
+							new DataEntity(DataLookup.ANIMATIONS.POKER_1.identifier).getRenderableContent(), 
+							null),
+					500);
+			break;
+		default:
+			break;
+		}
+	}
+
+	@Override public Image getRenderableContent() {
+		Image image = this.sceneRenderer.getRenderableContent();
+		if (image == null) {
+			image = super.getRenderableContent();
+		}
+
+		return image;
+	}
+
+	@Override public void refresh() {
+		setBackside();
+	}
 }
